ecec5884a7360fd51a654f937ce575afc2b1b38e
marek <maravondra@gmail.com>
2023-04-12 20:21:20 +0200

    init commit

---
+++ src/test/java/com/engeto/wedding/WeddingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.engeto.wedding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeddingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
